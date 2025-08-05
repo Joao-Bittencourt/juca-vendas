ff06ce43c138c7e0fc1462135b75a063678d3dee
joao bittencourt <joaogsbittencourt@gmail.com>
2025-08-05 12:16:51 -0300

    em desenv

--- resources/views/components/lists/base-list.blade.php
+++ resources/views/components/lists/base-list.blade.php
@@ -1,20 +1,17 @@
-@props(['data' => []])
+@props(['data' => [
+    ]])
 
 <div class="card-body">
     <div class="row">
         <div class="col-lg-12">
             <div class="main-box clearfix">
                 <div class="table-responsive">
-                    <table class="table">
+                    <table class="table table-sm table-hover table-centered table-nowrap mb-0">
                         <thead>
                             <tr>
                                 <th>{{ __('Cod') }}</th>
-                                <!-- <th>{{ __('Identifier') }}</th> -->
-
-                                <th class="text-center">{{ __('Payment method') }}</th>
-                                <th>{{ __('Type') }}</th>
-                                <th>{{ __('Installment number') }}</th>
-                                <th class="text-center">{{ __('Amount') }}</th>
+                                <th>{{ __('Brand')}}</th>
+                                <th class="text-center">{{ __('Status') }}</th>
                                 <th class="text-center">{{ __('Created at') }}</th>
                                 <th>&nbsp;</th>
                             </tr>
@@ -25,22 +22,14 @@
                                 <td>
                                     {{ $unit->id }}
                                 </td>
-                                <!-- <td>
-                                {{ $unit->identifier }}
-                            </td> -->
-                                <td class="text-center">
-                                    {{ $unit->paymentMethod->name ?? '-' }}
-                                </td>
                                 <td>
-                                    {{ __($unit->type) }}
-                                </td>
-                                <td>
-                                    {{ $unit->installment_number ?? '-' }}
+                                    {{ $unit->name }}
+
                                 </td>
                                 <td class="text-center">
-                                    {{ $unit->amount ? number_format($unit->amount, 2, ',', '.') :  '-' }}
+                                    {{ $unit->active == '1' ? __('Active') : __('Inactive') }}
                                 </td>
-                                <td class="text-center"> 
+                                <td class="text-center">
                                     {{ $unit->created_at->format('d/m/Y') }}
                                 </td>
                                 <td class="text-center">
